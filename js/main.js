caace7801eb680f6985191c448cd81ef57aa5047
aletomk <alejotomkiewicz1@gmail.com>
2023-03-31 17:54:05 -0300

    se creó la función del plazo fijo con interes compuesto

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let continuar = true;
+const TASA_MENSUAL = 6;
 
 function iniciarConsulta() {
-    const TASA_MENSUAL = 6;
     let capital = parseInt(prompt("💲Ingrese el capital a invertir\n¡¡Por favor sin letras, puntos ni espacios y mayor a $999!!").trim());
     if (!isNaN(capital) && (capital >= 1000)){
         let meses = prompt("📅Ingrese la cantidad de meses que usted desea (hasta 60)").trim();
@@ -9,9 +9,9 @@ function iniciarConsulta() {
                 let interes = (capital * (TASA_MENSUAL * meses))/100;
                 let dineroTotal = capital + interes;
                 if (meses == 1) {
-                alert("✅Ud. tendrá $" + dineroTotal + " en un plazo de " + meses + " mes");
+                alert(`✅Ud. tendrá \$${parseInt(dineroTotal)} en un plazo de ${meses} mes`);
                 } else{
-                    alert("✅Ud. tendrá $" + dineroTotal + " en un plazo de " + meses + " meses");
+                    alert(`✅Ud. tendrá \$${parseInt(dineroTotal)} en un plazo de ${meses} meses`);
                 }
             } else{
                 alert("⛔Los datos ingresados no son validos, por favor vuelve a intentarlo");
@@ -29,8 +29,35 @@ function plazoFijo() {
     alert("No hay problema, gracias por su visita😉");
 }
 
-function plazoFijoCompuesto() {
+function iniciarConsultaCompuesto() {
+    let capital = parseInt(prompt("💲Ingrese el capital a invertir\n¡¡Por favor sin letras, puntos ni espacios y mayor a $999!!").trim());
+    if (!isNaN(capital) && (capital >= 1000)){
+        let meses = prompt("📅Ingrese la cantidad de meses que usted desea (hasta 60)").trim();
+            if (meses >= 1 && meses <=60) {
+                for(let i = 1; i <= meses; i++){
+                    let interes = (capital * TASA_MENSUAL)/100;
+                    capital = capital + interes;
+                }
+                let dineroTotal = parseInt(capital);
+                if (meses == 1) {
+                alert(`✅Ud. tendrá \$${dineroTotal} en un plazo de ${meses} mes`);
+                } else{
+                    alert(`✅Ud. tendrá \$${dineroTotal} en un plazo de ${meses} meses`);
+                }
+            } else{
+                alert("⛔Los datos ingresados no son validos, por favor vuelve a intentarlo");
+            }            
+    } else{
+        alert("⛔Los datos ingresados no son validos, por favor vuelve a intentarlo");
+    }
+}
 
+function plazoFijoCompuesto() {
+    while(continuar){
+        iniciarConsultaCompuesto();
+        continuar = confirm("¿Deseas volver a probar con otro capital?");
+    }
+    alert("No hay problema, gracias por su visita😉");
 }
 
 
