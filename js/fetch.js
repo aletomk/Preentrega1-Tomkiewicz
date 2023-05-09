84344e6388a334798f128dbc13cdbf2cebd9d491
aletomk <alejotomkiewicz1@gmail.com>
2023-05-09 20:21:31 -0300

    se corrigió un error con fetch

--- js/fetch.js
+++ js/fetch.js
@@ -11,27 +11,27 @@ async function cotizacionDivisas() {
         const precioDolar = await dolar.json();
 
         if (precioDolar[0]["class-variacion"] == "up") {
-            dolarLI.innerHTML = `<img src="../images/sube.png"> <b>Dólar Oficial:</b> <span class="up">$${precioDolar[0].venta}</span>`;
+            dolarLI.innerHTML = `<img src="images/sube.png"> <b>Dólar Oficial:</b> <span class="up">$${precioDolar[0].venta}</span>`;
         } else {
-            dolarLI.innerHTML = `<img src="../images/baja.png"> <b>Dólar Oficial:</b> <span class="down">$${precioDolar[0].venta}</span>`;
+            dolarLI.innerHTML = `<img src="images/baja.png"> <b>Dólar Oficial:</b> <span class="down">$${precioDolar[0].venta}</span>`;
         }
         
         if (precioDolar[1]["class-variacion"] == "up") {
-            dolarBlueLI.innerHTML = `<img src="../images/sube.png"> <b>Dólar Blue:</b> <span class="up">$${precioDolar[1].venta}</span>`;
+            dolarBlueLI.innerHTML = `<img src="images/sube.png"> <b>Dólar Blue:</b> <span class="up">$${precioDolar[1].venta}</span>`;
         } else {
-            dolarBlueLI.innerHTML = `<img src="../images/baja.png"> <b>Dólar Blue:</b> <span class="down">$${precioDolar[1].venta}</span>`;
+            dolarBlueLI.innerHTML = `<img src="images/baja.png"> <b>Dólar Blue:</b> <span class="down">$${precioDolar[1].venta}</span>`;
         }
 
         if (precioDolar[2]["class-variacion"] == "up") {
-            dolarMayoristaLI.innerHTML = `<img src="../images/sube.png"> <b>Dólar Mayorista:</b> <span class="up">$${precioDolar[2].venta}</span>`;
+            dolarMayoristaLI.innerHTML = `<img src="images/sube.png"> <b>Dólar Mayorista:</b> <span class="up">$${precioDolar[2].venta}</span>`;
         } else {
-            dolarMayoristaLI.innerHTML = `<img src="../images/baja.png"> <b>Dólar Mayorista:</b> <span class="down">$${precioDolar[2].venta}</span>`;
+            dolarMayoristaLI.innerHTML = `<img src="images/baja.png"> <b>Dólar Mayorista:</b> <span class="down">$${precioDolar[2].venta}</span>`;
         }
 
         if (precioDolar[3]["class-variacion"] == "up") {
-            dolarAhorroLI.innerHTML = `<img src="../images/sube.png"> <b>Dólar Ahorro:</b> <span class="up">$${precioDolar[3].venta}</span>`;
+            dolarAhorroLI.innerHTML = `<img src="images/sube.png"> <b>Dólar Ahorro:</b> <span class="up">$${precioDolar[3].venta}</span>`;
         } else {
-            dolarAhorroLI.innerHTML = `<img src="../images/baja.png"> <b>Dólar Ahorro:</b> <span class="down">$${precioDolar[3].venta}</span>`;
+            dolarAhorroLI.innerHTML = `<img src="images/baja.png"> <b>Dólar Ahorro:</b> <span class="down">$${precioDolar[3].venta}</span>`;
         }
         
     } catch(error) {
